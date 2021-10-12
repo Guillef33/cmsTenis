c7bbe79bfdde07702017b9c77b2748324e761151
Guillermo <guillef33@gmail.com>
2021-10-12 13:53:26 -0300

    imagen en php

--- templates/include/article.php
+++ templates/include/article.php
@@ -1,19 +1,20 @@
-<?php foreach ( $results['articles'] as $article ) { ?>
+<?php foreach ($results['articles'] as $article) { ?>
 
-<li>
-  <div class="articleBody">
+  <li>
+    <div class="articleBody">
       <div class="article-top-data">
-
-        <img src="templates/images/<?php echo htmlspecialchars( $article->image ) ?>">    
-        <p class="pubDate"><?php echo date('j F', $article->publicationDate)?></p>
-        <p class="pubDate"><?php echo htmlspecialchars( $article->category )?></p>
-        <p class="pubDate"><?php echo htmlspecialchars( $article->author )?></p>
+        <img src="templates\images\nadal.jpg" <?php echo htmlspecialchars($article->image) ?>">
+        <p class="pubDate"><?php echo date('j F', $article->publicationDate) ?></p>
+        <p class="pubDate"><?php echo htmlspecialchars($article->category) ?></p>
+        <p class="pubDate"><?php echo htmlspecialchars($article->author) ?></p>
       </div>
       <h2>
-        <a href=".?action=viewArticle&amp;articleId=<?php echo $article->id?>"><?php echo htmlspecialchars( $article->title )?></a>
+        <a href=".?action=viewArticle&amp;articleId=<?php echo $article->id ?>"><?php echo htmlspecialchars($article->title) ?></a>
       </h2>
-      <p class="summary"><?php echo htmlspecialchars( $article->summary )?></p>          
-  </div>
-</li>
+      <p class="summary"><?php echo htmlspecialchars($article->summary) ?></p>
+    </div>
+  </li>
+
+
 
 <?php } ?>
